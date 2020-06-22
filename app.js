43e66ea48b5bcb850df7c135bc01515b537e057f
SunZul <tnzulaa@gmail.com>
2020-06-23 00:39:01 +0800

    Project iin ehnii huvisagchdiig uusgesen.

---
+++ app.js
@@ -0,0 +1,9 @@
+var activePlayer = 1;
+var scores = [0, 0];
+var roundScore = 0;
+var dice = Math.floor(Math.random() * 6 + 1);
+document.querySelector("#score-0").textContent = 0;
+document.querySelector("#score-1").textContent = 0;
+document.querySelector("#current-0").textContent = 0;
+document.querySelector("#current-1").textContent = 0;
+document.querySelector(".dice").style.display = "none";
