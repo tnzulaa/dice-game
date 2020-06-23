a7ba6f50a80f25e028ea4651a674c883a33ce17d
SunZul <tnzulaa@gmail.com>
2020-06-23 17:59:01 +0800

    shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
+// Тоглогчийн ээлжийг хадгалах хувьсагч, 1-р тоглогч 0, 2-р тоглогч 1 гэж тэмдэглэе.
 var activePlayer = 1;
+// Тоглогчдын цуглуулсан оноог хадгалах хувьсагч
 var scores = [0, 0];
+// Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
 var roundScore = 0;
-var dice = Math.floor(Math.random() * 6 + 1);
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
-document.querySelector(".dice").style.display = "none";
+//Шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй, 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
+//var diceNumber = Math.floor(Math.random() * 6 + 1);
+// Програм эхлэхэд бэлтгэе
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var diceNumber = Math.floor(Math.random() * 6 + 1);
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + diceNumber + ".png";
+  //alert("Шоог хаялаа " + diceNumber);
+});
